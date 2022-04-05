a97b08b312ad48e39de442d0f0381ee83ba6e64b
li-zehan <zehan.li@qq.com>
2022-04-05 21:38:42 +0800

    add typing

--- graph_retriever.py
+++ graph_retriever.py
@@ -1,3 +1,5 @@
+from typing import Optional, Tuple
+
 from transformers.configuration_bart import BartConfig
 from modeling_bart import PretrainedBartModel, BartStructureEncoder, _filter_out_falsey_values
 
