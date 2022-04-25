8a66ec03f0d671231a732a36aa0a03fd2fe28bc4
li-zehan <zehan.li@qq.com>
2022-04-25 22:05:26 +0800

    fix bug

--- scripts/generate_test.py
+++ scripts/generate_test.py
@@ -3,7 +3,7 @@ f_in = open('annotated_fb_data_test.txt', 'r')
 f_src = open('src-test.txt', 'w')
 f_tgt = open('tgt-test.txt', 'w')
 
-for line in f:
+for line in f_in:
     head, relation, tail, question = line.strip().split('\t')
     f_src.write(" ".join([head, relation, tail]) + "\n")
     f_tgt.write(question + "\n")
