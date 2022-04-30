9321902ccdcb56404bad8c701745d6efe93af437
li-zehan <zehan.li@qq.com>
2022-04-30 16:47:08 +0800

    add src tgt

--- scripts/process_sq.py
+++ scripts/process_sq.py
@@ -1,40 +1,5 @@
 import json
-
-def www2fb(in_str):
-    if in_str.startswith("www.freebase.com"):
-        in_str = 'fb:%s' % (in_str.split('www.freebase.com/')[-1].replace('/', '.'))
-    if in_str == 'fb:m.07s9rl0':
-        in_str = 'fb:m.02822'
-    if in_str == 'fb:m.0bb56b6':
-        in_str = 'fb:m.0dn0r'
-    # Manual Correction
-    if in_str == 'fb:m.01g81dw':
-        in_str = 'fb:m.01g_bfh'
-    if in_str == 'fb:m.0y7q89y':
-        in_str = 'fb:m.0wrt1c5'
-    if in_str == 'fb:m.0b0w7':
-        in_str = 'fb:m.0fq0s89'
-    if in_str == 'fb:m.09rmm6y':
-        in_str = 'fb:m.03cnrcc'
-    if in_str == 'fb:m.0crsn60':
-        in_str = 'fb:m.02pnlqy'
-    if in_str == 'fb:m.04t1f8y':
-        in_str = 'fb:m.04t1fjr'
-    if in_str == 'fb:m.027z990':
-        in_str = 'fb:m.0ghdhcb'
-    if in_str == 'fb:m.02xhc2v':
-        in_str = 'fb:m.084sq'
-    if in_str == 'fb:m.02z8b2h':
-        in_str = 'fb:m.033vn1'
-    if in_str == 'fb:m.0w43mcj':
-        in_str = 'fb:m.0m0qffc'
-    if in_str == 'fb:m.07rqy':
-        in_str = 'fb:m.0py_0'
-    if in_str == 'fb:m.0y9s5rm':
-        in_str = 'fb:m.0ybxl2g'
-    if in_str == 'fb:m.037ltr7':
-        in_str = 'fb:m.0qjx99s'
-    return in_str
+from utils import www2fb
 
 
 def load_mid2name(filename: str):
@@ -54,6 +19,10 @@ def load_mid2name(filename: str):
 
 def convert_ids_to_names(src: str, tgt: str, mid2name):
     dataset = []
+
+    f_src = open('src-test.txt', 'w')
+    f_tgt = open('tgt-test.txt', 'w')
+
     with open(src, 'r') as f_in:
         qid = 0
         for line in  f_in:
@@ -86,8 +55,14 @@ def convert_ids_to_names(src: str, tgt: str, mid2name):
             dataset.append(sample)
             qid += 1
 
+            f_src.write("\t".join([head_name, relation_name, tail_name]) + "\n")
+            f_tgt.write(question + '\n')
+
     with open(tgt, 'w') as f_out:
         json.dump(dataset, f_out)
+    
+    f_src.close()
+    f_tgt.close()
 
 
 if __name__ == "__main__":
