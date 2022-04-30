2a36bf9bfcc8349223b3dae027fb91fe56a387e8
li-zehan <zehan.li@qq.com>
2022-04-30 16:39:53 +0800

    add sq mid 2 name script

--- scripts/process_sq.py
+++ scripts/process_sq.py
@@ -1,15 +1,54 @@
 import json
 
+def www2fb(in_str):
+    if in_str.startswith("www.freebase.com"):
+        in_str = 'fb:%s' % (in_str.split('www.freebase.com/')[-1].replace('/', '.'))
+    if in_str == 'fb:m.07s9rl0':
+        in_str = 'fb:m.02822'
+    if in_str == 'fb:m.0bb56b6':
+        in_str = 'fb:m.0dn0r'
+    # Manual Correction
+    if in_str == 'fb:m.01g81dw':
+        in_str = 'fb:m.01g_bfh'
+    if in_str == 'fb:m.0y7q89y':
+        in_str = 'fb:m.0wrt1c5'
+    if in_str == 'fb:m.0b0w7':
+        in_str = 'fb:m.0fq0s89'
+    if in_str == 'fb:m.09rmm6y':
+        in_str = 'fb:m.03cnrcc'
+    if in_str == 'fb:m.0crsn60':
+        in_str = 'fb:m.02pnlqy'
+    if in_str == 'fb:m.04t1f8y':
+        in_str = 'fb:m.04t1fjr'
+    if in_str == 'fb:m.027z990':
+        in_str = 'fb:m.0ghdhcb'
+    if in_str == 'fb:m.02xhc2v':
+        in_str = 'fb:m.084sq'
+    if in_str == 'fb:m.02z8b2h':
+        in_str = 'fb:m.033vn1'
+    if in_str == 'fb:m.0w43mcj':
+        in_str = 'fb:m.0m0qffc'
+    if in_str == 'fb:m.07rqy':
+        in_str = 'fb:m.0py_0'
+    if in_str == 'fb:m.0y9s5rm':
+        in_str = 'fb:m.0ybxl2g'
+    if in_str == 'fb:m.037ltr7':
+        in_str = 'fb:m.0qjx99s'
+    return in_str
+
 
 def load_mid2name(filename: str):
     mid2name = {}
     with open(filename, 'r') as f:
         for line in f:
-            data = line.strip().split('\t')
-            if len(data) != 2:
+            items = line.strip().split('\t')
+            if len(items) != 3:
                 continue
-            mid, name = data
-            mid2name[mid] = name
+            mid, type, name= items
+            if mid not in mid2name:
+                mid2name[mid] = [name]
+            else:
+                mid2name[mid].append(name)
     return mid2name
 
 
@@ -19,12 +58,12 @@ def convert_ids_to_names(src: str, tgt: str, mid2name):
         qid = 0
         for line in  f_in:
             head, relation, tail, question = line.strip().split('\t')
-            head_mid = '/'.join([""] + head.split('/')[1:])
-            tail_mid = '/'.join([""] + tail.split('/')[1:])
+            head_mid = www2fb(head)
+            tail_mid = www2fb(tail)
 
             relation_name = " ".join(sum([rel.split('_') for rel in relation.split('/')[1:]], []))
-            head_name = mid2name[head_mid] if head_mid in mid2name else 'none'
-            tail_name = mid2name[tail_mid] if tail_mid in mid2name else 'none'
+            head_name = mid2name[head_mid][0] if head_mid in mid2name else 'none'
+            tail_name = mid2name[tail_mid][0] if tail_mid in mid2name else 'none'
             
             sample = {
                 "id": qid,
@@ -52,7 +91,7 @@ def convert_ids_to_names(src: str, tgt: str, mid2name):
 
 
 if __name__ == "__main__":
-    mid2name = load_mid2name('mid2name.tsv')
+    mid2name = load_mid2name('names.trimmed.2M.txt')
     convert_ids_to_names('annotated_fb_data_train.txt', 'train.json', mid2name)
     convert_ids_to_names('annotated_fb_data_valid.txt', 'dev.json', mid2name)
     convert_ids_to_names('annotated_fb_data_test.txt', 'test.json', mid2name)
